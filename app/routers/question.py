eac8ee0c985a342ba62b0adab1dcc5f2f2aa4e9a
faradawn <faradawny@gmail.com>
2024-06-28 04:15:49 +0000

    add get top vote sample api

--- app/routers/question.py
+++ app/routers/question.py
@@ -187,3 +187,12 @@ async def submit_answer(request: SubmitAnswerRequest):
             status="success",
             message=f"Incorrect. User answer: {request.user_answer}, correct answer: {request.correct_answer}"
         )
+
+class QRequest(BaseModel):
+    question_slug: str
+class QRes(BaseModel):
+    res: str
+
+@router.post("/get_top_vote", response_model=QRes)
+async def submit_answer(request: QRequest):
+    return QRes(res="Answer 1: The main idea is the same with problem Linked List Cycle II,https://leetcode.com/problems/linked-list-cycle-ii/. Use two pointers the fast and the slow. The fast one goes forward two steps each time, while the slow one goes only step each time. They must meet the same item when slow==fast. In fact, they meet in a circle, the duplicate number must be the entry point of the circle when visiting the array from nums[0]. Next we just need to find the entry point. We use a point(we can use the fast one before) to visit form begining with one step each time, do the same job to slow. When fast==slow, they meet at the entry point of the circle. The easy understood code is as follows. ")
